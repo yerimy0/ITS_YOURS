06b43081afdf5f72db2b8c4f66a048b939642313
임예림 <dpflachlrh@gmail.com>
2024-04-04 15:20:55 +0900

    FEAT:로그인페이지 기능 추가

--- frontend/vite-project/src/pages/Login/Login.jsx
+++ frontend/vite-project/src/pages/Login/Login.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import { LoginWrapper, Logo } from './LoginStyles.jsx';
-import DynamicForm from './DynamicForm.jsx'; 
-import SocialLoginOptions from './SocialLoginOptions.jsx';
+import LoginForm from '../../components/LoginForm.jsx';
+import SocialLoginOptions from '../../components/SocialLoginOptions.jsx';
 import AdditionalOptions from './AdditionalOptions.jsx';
 
 const Login = () => {
   return (
     <LoginWrapper>
       <Logo src="/logoText.png" alt="로고" />
-      <DynamicForm
-        inputPlaceholder1="아이디를 입력해주세요"
-        inputPlaceholder2="비밀번호를 입력해주세요"
-        buttonText="로그인"
-      />
+      <LoginForm />
       <AdditionalOptions options={[
         { label: "아이디찾기", path: "/find-id" },
         { label: "비밀번호찾기", path: "/find-password" },

c61c762101f54e6247c851275de534a24b412708
goeun kim <rlarhdms6828@naver.com>
2024-04-05 15:29:12 +0900

    FEAT: FE 상품 수정

--- frontend/vite-project/src/main.jsx
+++ frontend/vite-project/src/main.jsx
@@ -16,6 +16,7 @@ const router = createBrowserRouter([
       {path: "/home", element: <Home/>}, 
       {path: "/product/write", element: <ProductWrite />},
       {path: "/product", element: <Product />},
+      {path: "/product/edit/:id", element: <ProductWrite />}
     ]}, {path: "login", element: <Login/>}
   ]);
 

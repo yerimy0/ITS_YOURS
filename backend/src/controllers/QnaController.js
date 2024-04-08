625b7e6fa7f59838fb8800490cd07154256cd8e8
[SW8]이정은 <wienker0426@gmail.com>
2024-04-08 17:31:04 +0900

    CHORE:Eslint 설정

--- backend/src/controllers/QnaController.js
+++ backend/src/controllers/QnaController.js
@@ -3,16 +3,11 @@ const qnaService = require("../services/QnaService");
 const createQna = async (req, res, next) => {
   try {
     const { title, content } = req.body;
-    const result = await qnaService.createQna(title, content);
+    const result = await qnaService.createQna({title, content});
 
     if (!result) {
       throw new Error("Q&A를 쓸 권한이 없습니다.");
     }
-
-    const newQna = await qnaService.createQna({
-      title,
-      content,
-    });
     console.log(newQna);
     return res.status(200).json(newQna);
   } catch (err) {

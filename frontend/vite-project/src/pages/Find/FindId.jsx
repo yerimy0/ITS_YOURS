4f3d7d1a7bffb442197bc4da362a8fe0ada0519a
goeun kim <rlarhdms6828@naver.com>
2024-04-09 02:29:11 +0900

    Fix: 충돌 및 오류 해결

--- frontend/vite-project/src/pages/Find/FindId.jsx
+++ frontend/vite-project/src/pages/Find/FindId.jsx
@@ -4,19 +4,7 @@ import FindIdForm from '../../components/pages/Find/FindIdForm';
 import AdditionalOptions from '../../components/Users/AdditionalOptions';
 import LogoLink from '../../components/Users/LogoLink';
 
-<<<<<<< HEAD
-function FindId () {
-  return (
-    <Wrapper>
-      <LogoLink src="/logoText.png" />
-      <FindIdForm />
-      <AdditionalOptions options={[
-        { label: "비밀번호찾기", path: "/findpassword" },
-      ]} />
-    </Wrapper>
-  );
-=======
-const FindId = () => {
+function FindId() {
 	return (
 		<Wrapper>
 			<LogoLink src="/logoText.png" />
@@ -24,7 +12,6 @@ const FindId = () => {
 			<AdditionalOptions options={[{ label: '비밀번호찾기', path: '/findpassword' }]} />
 		</Wrapper>
 	);
->>>>>>> 32f572318d96e884c65818d0a73d8d6c8a5c833d
-};
+}
 
 export default FindId;

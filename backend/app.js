30781d5a53e7f616feb0532dd49787ff3f8e18b1
[SW8]이정은 <wienker0426@gmail.com>
2024-04-09 15:25:18 +0900

    FIX:OrderRouter 삭제

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,7 @@ const logger = require('morgan');
 
 const memberRouter = require('./src/routes/MemberRouter');
 const productsRouter = require('./src/routes/ProductsRouter');
-const orderRouter = require('./src/routes/OrderRouter');
-
 const qnaRouter = require('./src/routes/QnaRouter');
-
 const postRouter = require('./src/routes/PostRouter');
 
 const mongoose = require('mongoose');
@@ -28,7 +25,6 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/qna', qnaRouter);
-app.use('/api/orders', orderRouter);
 app.use('/api/members', memberRouter);
 app.use('/api/products', productsRouter);
 app.use('/api/community', postRouter);

9fbf5017623f7a979c7d032fb9d2dc29eff25a70
[SW8]이정은 <wienker0426@gmail.com>
2024-04-11 11:32:44 +0900

    FEAT:찜개수 1 증가 로직 추가

--- backend/src/services/WishesService.js
+++ backend/src/services/WishesService.js
@@ -1,16 +1,32 @@
-const { Wishes } = require('../models');
+const { Wishes, Products } = require('../models');
 
 async function toggleWish(userId, productId) {
 	const existingWish = await Wishes.findOne({ userId, productId });
 	if (existingWish) {
 		// 이미 찜을 했다면 삭제
 		await Wishes.deleteOne({ _id: existingWish._id });
-		return { message: '찜 목록에서 삭제되었습니다.', deleted: true };
+
+		// 찜 개수 줄이기 전에 Products에서 해당 상품 찾기
+		const product = await Products.findById(productId);
+		// 만약 wishesCount가 0보다 크다면, 1 감소시키기
+		if (product.wishesCount > 0) {
+			await Products.findByIdAndUpdate(productId, { $inc: { wishesCount: -1 } });
+		}
+
+		// 업데이트된 상품 정보 반환
+		const updatedProduct = await Products.findById(productId);
+		return updatedProduct;
 	} else {
 		// 찜을 하지 않았다면 추가
-		const newWishes = new Wishes({ userId, productId });
-		await newWishes.save();
-		return { message: '찜 목록에 추가되었습니다.', added: true };
+		const newWish = new Wishes({ userId, productId });
+		await newWish.save();
+
+		// products 스키마의 wishesCount 1 증가
+		await Products.findByIdAndUpdate(productId, { $inc: { wishesCount: 1 } });
+
+		// 업데이트된 상품 정보 반환
+		const updatedProduct = await Products.findById(productId);
+		return updatedProduct;
 	}
 }
 

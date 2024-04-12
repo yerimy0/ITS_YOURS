a1a912f292eeb28108799fd154f740c0f50c1941
goeun kim <rlarhdms6828@naver.com>
2024-04-12 15:00:22 +0900

    FEAT: FE instance 토큰값 넣기

--- frontend/vite-project/src/apis/service/product.api.js
+++ frontend/vite-project/src/apis/service/product.api.js
@@ -1,18 +1,15 @@
 import axios from 'axios';
+import instance from '../axiosInstance';
 
 export async function Register(register) {
 	try {
 		// 쿠키에서 authToken 값을 가져옵니다.
-		const authToken = document.cookie
-			.split('; ')
-			.find(row => row.startsWith('authToken='))
-			.split('=')[1];
+		// const authToken = document.cookie
+		// 	.split('; ')
+		// 	.find(row => row.startsWith('authToken='))
+		// 	.split('=')[1];
 
-		const writing = await axios.post('/api/api/products', register, {
-			headers: {
-				Authorization: authToken ? `Bearer ${authToken}` : '', // 쿠키에 authToken 값이 있으면 Bearer 토큰과 함께 Authorization 헤더에 추가합니다.
-			},
-		});
+		const writing = await instance.post('/api/products', register);
 		console.log(writing);
 	} catch (err) {
 		console.log(err.message);

40631fba584e2a66369acf58b4261f0997776f74
everyoon <jy.k7377@gmail.com>
2024-04-12 12:32:25 +0900

    FEAT: 검색창 기능 구현

--- frontend/vite-project/src/components/pages/Product/ProductsContainer.jsx
+++ frontend/vite-project/src/components/pages/Product/ProductsContainer.jsx
@@ -1,32 +1,55 @@
 import React, { useEffect, useState } from 'react';
 import ProductCard from '../../ProductCard';
-import { Loading, ProductsWrap, Products } from './ProductsContainerStyle';
+import {
+	ProductsWrap,
+	Products,
+	NoProductsMessageWrap,
+	NoProductsMessage,
+	Searchrecommendation,
+} from './ProductsContainerStyle';
 import Paginator from '../../Paginator';
 import instance from '../../../apis/axiosInstance';
 import ProductHeader from './ProductHeader';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 function ProductsContainer() {
+	const navigate = useNavigate();
+	const location = useLocation();
+
 	const [products, setProducts] = useState([]);
 	const [sortedProducts, setSortedProducts] = useState([]);
+	const [searchResults, setSearchResults] = useState([]);
 	const itemsPerPage = 20;
 	const [currentPage, setCurrentPage] = useState(0);
 	const [totalItems, setTotalItems] = useState(0);
-	const navigate = useNavigate();
 
 	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const res = await instance.get('/api/products/list');
-				setProducts(res.data);
-				setSortedProducts(res.data);
-				setTotalItems(res.data.length);
-			} catch (error) {
-				console.error('상품 데이터를 불러오는 중 에러 발생:', error);
-			}
-		};
-		fetchData();
-	}, []);
+		if (location.state && location.state.searchResults) {
+			setSearchResults(location.state.searchResults);
+			setSortedProducts(location.state.searchResults);
+			setTotalItems(location.state.searchResults.length);
+		} else {
+			fetchDefaultProducts();
+		}
+	}, [location.state]);
+
+	const fetchDefaultProducts = async () => {
+		try {
+			const res = await instance.get('/api/products/list');
+			setProducts(res.data);
+			setSortedProducts(res.data);
+			setTotalItems(res.data.length);
+		} catch (error) {
+			console.error('상품 데이터를 불러오는 중 에러 발생:', error);
+		}
+	};
+
+	// 검색 결과 처리
+	const handleSearchResults = results => {
+		setSearchResults(results);
+		setSortedProducts(results);
+		setTotalItems(results.length);
+	};
 
 	//정렬 옵션에 따른 제품 정렬
 	const handleSortChange = sortOption => {
@@ -58,8 +81,23 @@ function ProductsContainer() {
 
 	return (
 		<>
-			<ProductHeader onSortChange={handleSortChange} onFilterChange={handleFilterChange} />
+			<ProductHeader
+				onSortChange={handleSortChange}
+				onFilterChange={handleFilterChange}
+				onSearchResults={handleSearchResults}
+				totalItems={totalItems}
+			/>
 			<ProductsWrap>
+				{searchResults.length === 0 && (
+					<NoProductsMessageWrap>
+						<NoProductsMessage>검색결과를 찾지 못했어요.</NoProductsMessage>
+						<Searchrecommendation>
+							- 단어의 철자가 정확한지 확인해 보세요 <br />
+							- 보다 일반적인 검색어로 다시 검색해 보세요 <br />
+							-검색어의 띄어쓰기를 다르게 해보세요 <br />- 유해/금지어가 아닌지 확인해주세요
+						</Searchrecommendation>
+					</NoProductsMessageWrap>
+				)}
 				<Products>
 					{productsToShow.map(product => (
 						<ProductCard
@@ -71,13 +109,15 @@ function ProductsContainer() {
 						/>
 					))}
 				</Products>
+				{searchResults.length > 0 && (
+					<Paginator
+						currentPage={currentPage}
+						totalItems={totalItems}
+						itemsCountPerPage={itemsPerPage}
+						onChange={handlePageChange}
+					/>
+				)}
 			</ProductsWrap>
-			<Paginator
-				currentPage={currentPage}
-				totalItems={totalItems}
-				itemsCountPerPage={itemsPerPage}
-				onChange={handlePageChange}
-			/>
 		</>
 	);
 }
--- frontend/vite-project/src/components/pages/Product/ProductsContainerStyle.jsx
+++ frontend/vite-project/src/components/pages/Product/ProductsContainerStyle.jsx
@@ -1,25 +1,55 @@
 import styled from 'styled-components';
 
-const Loading = styled.div`
-	font-size: 24px;
-	line-height: 28px;
-	margin: 0;
-`;
 const ProductsWrap = styled.section`
 	display: flex;
 	max-width: 1200px;
 	width: 100%;
 	margin: 0 auto;
+	flex-direction: column;
 `;
 
 const Products = styled.div`
 	display: flex;
 	flex-wrap: wrap;
-	align-items: center;
+	align-items: self-start;
 	align-content: center;
 	gap: 50px;
-	justify-content: space-evenly;
+	justify-content: flex-start;
 	margin: 60px 0 30px 0;
 `;
 
-export { Loading, ProductsWrap, Products };
+const NoProductsMessageWrap = styled.div`
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	-webkit-box-align: center;
+	align-items: center;
+	padding: 4rem 0px 8rem;
+`;
+const NoProductsMessage = styled.div`
+	width: 500px;
+	font-weight: 600;
+	border-bottom: 1px solid #f7f2fa;
+	display: flex;
+	flex-direction: column;
+	-webkit-box-align: center;
+	align-items: center;
+	padding-bottom: 35px;
+	font-size: 24px;
+	font-style: normal;
+	font-weight: 400;
+	line-height: 32px;
+	margin-bottom: 20px;
+`;
+
+const Searchrecommendation = styled.div`
+	text-align: center;
+	padding-top: 40px;
+	font-size: 12px;
+	font-style: normal;
+	font-weight: 500;
+	line-height: 1.9;
+	letter-spacing: 0.5px;
+`;
+
+export { ProductsWrap, Products, NoProductsMessageWrap, NoProductsMessage, Searchrecommendation };

d9fe1babbbe15c1fe0106620d3e3ca4c518f1876
임예림 <dpflachlrh@gmail.com>
2024-04-14 01:55:32 +0900

    FEAT: QNA 등록, 조회 API 연동

--- frontend/vite-project/src/components/pages/Mypage/AskSupportList/InquiryItem.jsx
+++ frontend/vite-project/src/components/pages/Mypage/AskSupportList/InquiryItem.jsx
@@ -1,35 +1,44 @@
 import React from 'react';
-import axios from 'axios';
 import { InquiryItem as StyledInquiryItem, Content, ActionContainer } from './AskSupportListStyles';
 import DeleteButton from './DeleteButton';
+import { useNavigate } from 'react-router-dom';
 import ToggleContentButton from './ToggleContentButton';
 import InquiryStatus from './InquiryStatus';
+import { deleteInquiry } from '../../../../apis/service/AskSupportApi';
 
-function InquiryItem({ inquiry, toggleContent, deleteInquiry }) {
-	function handleDelete(inquiryId) {
-		axios
-			.delete(`/api/qna/${inquiryId}`)
-			.then(() => {
-				deleteInquiry(inquiryId);
-				alert('문의가 성공적으로 삭제되었습니다.');
-			})
-			.catch(error => {
-				console.error('Failed to delete the inquiry:', error);
-				alert('문의 삭제에 실패했습니다.');
-			});
+function InquiryItem({ inquiry, toggleContent, deleteInquiryFromList }) {
+	const navigate = useNavigate();
+
+	async function handleDelete() {
+		const { _id } = inquiry;
+		if (!_id) {
+			console.error('Error: The inquiry ID is undefined or invalid.');
+			alert('문의 삭제에 실패했습니다. 유효하지 않은 문의 ID입니다.');
+			return;
+		}
+
+		try {
+			await deleteInquiry(_id);
+			alert('문의가 성공적으로 삭제되었습니다.');
+			deleteInquiryFromList(_id);
+			navigate('/inquiry-list');
+		} catch (error) {
+			console.error('Failed to delete the inquiry:', error);
+			alert('문의 삭제에 실패했습니다.');
+		}
 	}
 
-	const { id, title, content, status, show } = inquiry;
+	const { _id, title, content, status, show } = inquiry;
 
 	return (
 		<StyledInquiryItem>
 			<div>
-				<ToggleContentButton onClick={() => toggleContent(id)} title={title} />
+				<ToggleContentButton onClick={() => toggleContent(_id)} title={title} />
 				<Content show={show}>{content}</Content>
 			</div>
 			<ActionContainer>
 				<InquiryStatus status={status} />
-				<DeleteButton onClick={() => handleDelete(id)} />
+				<DeleteButton onClick={handleDelete} />
 			</ActionContainer>
 		</StyledInquiryItem>
 	);
--- frontend/vite-project/src/components/pages/Mypage/AskSupportList/InquiryList.jsx
+++ frontend/vite-project/src/components/pages/Mypage/AskSupportList/InquiryList.jsx
@@ -1,29 +1,35 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import InquiryItem from './InquiryItem';
 import { InquiryList as StyledInquiryList } from './AskSupportListStyles';
+import { fetchInquiries } from '../../../../apis/service/AskSupportApi';
 
 function InquiryList({ toggleContent, deleteInquiry }) {
 	const [inquiries, setInquiries] = useState([]);
 
 	useEffect(() => {
-		const fetchInquiries = async () => {
+		const fetchData = async () => {
 			try {
-				const response = await axios.get('/api/qna');
-				setInquiries(response.data);
+				const response = await fetchInquiries();
+				if (response && Array.isArray(response.data)) {
+					setInquiries(response.data);
+				} else {
+					console.error('반환된 데이터가 배열이 아닙니다:', response);
+					setInquiries([]);
+				}
 			} catch (error) {
-				console.error('Failed to fetch inquiries:', error);
+				console.error('문의사항을 가져오는데 실패했습니다:', error);
+				setInquiries([]);
 			}
 		};
 
-		fetchInquiries();
-	}, []); // 컴포넌트가 마운트될 때 한 번만 호출
+		fetchData();
+	}, []);
 
 	return (
 		<StyledInquiryList>
 			{inquiries.map(inquiry => (
 				<InquiryItem
-					key={inquiry.id}
+					key={inquiry._id}
 					inquiry={inquiry}
 					toggleContent={toggleContent}
 					deleteInquiry={deleteInquiry}

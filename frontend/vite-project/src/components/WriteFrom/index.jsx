10f9ef33822703ca45763798f215f79790a3571a
goeun kim <rlarhdms6828@naver.com>
2024-04-17 04:47:46 +0900

    FIX: FE 상품 등록, 수정 오류 해결

--- frontend/vite-project/src/components/WriteFrom/index.jsx
+++ frontend/vite-project/src/components/WriteFrom/index.jsx
@@ -17,6 +17,7 @@ import { Section, Section2, Section3, Section4 } from './components/Section';
 import { useEffect, useState, createContext } from 'react';
 import { Register } from '../../apis/service/product.api';
 
+export const SetRegisterContext = createContext();
 export const RegisterContext = createContext();
 
 function WriteForm() {
@@ -120,55 +121,57 @@ function WriteForm() {
 	}
 
 	return (
-		<RegisterContext.Provider value={{ setRegister, register }}>
-			<RegisterBox>
-				<Title>
-					<TopTitle>상품 등록</TopTitle>
-					<RedStar>*필수 항목</RedStar>
-				</Title>
-				<Line>
-					<hr />
-				</Line>
-				<MainContent>
-					<InputImg onImageChange={handleImageChange} value={imgUrls} />
-					<Section label={'도서명'} onChange={onChange} value={name} name="name" />
-					<Section label={'판매가'} onChange={onChange} value={price} name="price" />
-					<ProductTwoInput>
-						<Section2 label={'출판사'} onChange={onChange} value={publisher} name="publisher" />
-						<Section2 label={'저자'} onChange={onChange} value={author} name="author" />
-					</ProductTwoInput>
-					<Section3
-						label={'상품 설명'}
-						onChange={onChange}
-						value={description}
-						name="description"
-					/>
-					<Section4 value={condition} />
-				</MainContent>
-				<RegButtons>
-					<BigButton className="Button" onClick={tempSave}>
-						임시저장
-					</BigButton>{' '}
-					<BigButton
-						className="Button"
-						onClick={() => {
-							if (isValid) {
-								Upload();
-							} else {
-								alert('모든 필수 항목을 입력해주세요!');
-							}
-						}}
-					>
-						등록하기
-					</BigButton>
-				</RegButtons>
-				<Alert>
-					* 시크릿 브라우징 모드와 같은 환경 등에서는 임시저장 기능을 사용할 수 없습니다. 환경을
-					확인해주십시오! <br />
-					또한, 임시저장이 되지 않는 경우, 쿠키를 지우고 다시 시도하세요!
-				</Alert>
-			</RegisterBox>
-		</RegisterContext.Provider>
+		<SetRegisterContext.Provider value={setRegister}>
+			<RegisterContext.Provider value={register}>
+				<RegisterBox>
+					<Title>
+						<TopTitle>상품 등록</TopTitle>
+						<RedStar>*필수 항목</RedStar>
+					</Title>
+					<Line>
+						<hr />
+					</Line>
+					<MainContent>
+						<InputImg onImageChange={handleImageChange} value={imgUrls} />
+						<Section label={'도서명'} onChange={onChange} value={name} name="name" />
+						<Section label={'판매가'} onChange={onChange} value={price} name="price" />
+						<ProductTwoInput>
+							<Section2 label={'출판사'} onChange={onChange} value={publisher} name="publisher" />
+							<Section2 label={'저자'} onChange={onChange} value={author} name="author" />
+						</ProductTwoInput>
+						<Section3
+							label={'상품 설명'}
+							onChange={onChange}
+							value={description}
+							name="description"
+						/>
+						<Section4 value={condition} />
+					</MainContent>
+					<RegButtons>
+						<BigButton className="Button" onClick={tempSave}>
+							임시저장
+						</BigButton>{' '}
+						<BigButton
+							className="Button"
+							onClick={() => {
+								if (isValid) {
+									Upload();
+								} else {
+									alert('모든 필수 항목을 입력해주세요!');
+								}
+							}}
+						>
+							등록하기
+						</BigButton>
+					</RegButtons>
+					<Alert>
+						* 시크릿 브라우징 모드와 같은 환경 등에서는 임시저장 기능을 사용할 수 없습니다. 환경을
+						확인해주십시오! <br />
+						또한, 임시저장이 되지 않는 경우, 쿠키를 지우고 다시 시도하세요!
+					</Alert>
+				</RegisterBox>
+			</RegisterContext.Provider>
+		</SetRegisterContext.Provider>
 	);
 }
 

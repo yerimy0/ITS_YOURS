de13c6267fa16653757cdc71a08a95255f939b09
[SW8]이정은 <wienker0426@gmail.com>
2024-04-17 20:23:39 +0900

    FEAT:임시주석 추가

--- backend/app.js
+++ backend/app.js
@@ -28,6 +28,7 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+//
 const corsOptions = {
 	origin: ['http://localhost:5173', 'http://34.47.86.42'],
 	credentials: true,

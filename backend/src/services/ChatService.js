185d462e5c00140d04ea14c2ef9fc09984f32920
goeun kim <rlarhdms6828@naver.com>
2024-04-18 16:22:10 +0900

    FEAT: BE 채팅 중간 커밋

--- backend/src/services/ChatService.js
+++ backend/src/services/ChatService.js
@@ -6,7 +6,7 @@ const { Chatroom } = require('../models');
  * 작성 시작일 : 2024-04-17
  * 채팅방 생성시 동작되는 DB작업을 모아놓은 service입니다.
  */
-async function createChatroom(productId, sellerId, buyerId) {
+async function createChatroom(buyerId, productId, sellerId) {
 	const newRoomData = {
 		productId: productId,
 		sellerId: sellerId,

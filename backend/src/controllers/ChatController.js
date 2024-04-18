81a86205e89e17889ce02b76d37f07d15bbfd13d
[SW8]이정은 <wienker0426@gmail.com>
2024-04-18 22:23:46 +0900

    임시커밋

--- backend/src/controllers/ChatController.js
+++ backend/src/controllers/ChatController.js
@@ -21,6 +21,9 @@ const createChatroom = async (req, res, next) => {
 		const buyerId = req.user._id;
 		const { productId, sellerId } = req.params;
 
+		if (!buyerId) {
+			throw new BadRequestError('로그인 후 이용해주세요.');
+		}
 		const newChatroom = await chatService.createChatroom(buyerId, productId, sellerId);
 
 		res.status(200).json(newChatroom);
@@ -38,17 +41,47 @@ const getChatroomList = async (req, res, next) => {
 		if (!memberId) {
 			throw new BadRequestError('로그인 후 이용해주세요.');
 		}
+
 		const chatList = await chatService.getChatroomList(memberObjectId);
+
+		// 사용자의 권한 확인
+		const isAuthorized = chatList.some(
+			chatroom =>
+				chatroom.buyerId.toString() === memberId.toString() ||
+				chatroom.sellerId.toString() === memberId.toString(),
+		);
+
+		if (!isAuthorized) {
+			throw new UnauthorizedError('권한이 없습니다.');
+		}
 
 		res.status(200).json(chatList);
 	} catch (err) {
 		next(err);
 	}
 };
-
+//채팅방 메세지 저장
 const saveChatMessage = async (req, res, next) => {};
+
+//칭찬하기
+const giveGoodManners = async (req, res, next) => {
+	try {
+		const { like } = req.params;
+
+		// 멤버의 좋아요 수 업데이트
+		await memberService.updateLikeCount(like);
+
+		res.status(200).json({ message: '좋아요가 반영되었습니다.' });
+	} catch (err) {
+		next(err);
+	}
+};
+//비판하기
+
+//구매확정
 module.exports = {
 	createChatroom,
 	getChatroomList,
 	saveChatMessage,
+	giveGoodManners,
 };

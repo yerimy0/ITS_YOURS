e3a0d04baba94926b22e765aa86c82dce11e79a7
[SW8] 류효종 <hj4965@gmail.com>
2024-04-18 10:06:12 +0900

    FEAT: 라우터 이미지업로드 미들웨어 추가

--- backend/src/routes/PostRouter.js
+++ backend/src/routes/PostRouter.js
@@ -11,13 +11,13 @@ const {
 const router = Router();
 
 // 커뮤니티 - 게시글 글 작성
-router.post('/posts', validateToken, createPost);
+router.post('/posts', upload.single('img1'), validateToken, createPost);
 // 커뮤니티 - 게시글 글 목록 조회
 router.get('/posts', getAllPosts);
 // 커뮤니티 - 게시글 상세 글 조회
 router.get('/posts/:postId', getPostDetails);
 // 커뮤니티 - 게시글 수정
-router.put('/posts/:postId/', validateToken, updatePost);
+router.put('/posts/:postId/', upload.single('img1'), validateToken, updatePost);
 // 커뮤니티 - 게시글 삭제
 router.delete('/posts/:postId', validateToken, deletePost);
 

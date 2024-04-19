c2db02b6f64720436e62ca0d37d0911b4997829a
임예림 <dpflachlrh@gmail.com>
2024-04-19 22:52:08 +0900

    FIX: 이메일 인증 ALERT 오류 해결

--- frontend/vite-project/src/components/Users/EmailVerificationForm.jsx
+++ frontend/vite-project/src/components/Users/EmailVerificationForm.jsx
@@ -42,9 +42,6 @@ function EmailVerificationForm({
 			} else if (error.request) {
 				// 요청이 전송되었지만 응답을 받지 못한 경우
 				alert('네트워크 오류가 발생했습니다. 다시 시도해주세요.');
-			} else {
-				// 기타 오류
-				alert('알 수 없는 오류가 발생했습니다. 다시 시도해주세요.');
 			}
 			setEmailVerified(false); // 오류 발생 시 상태 초기화
 		}
